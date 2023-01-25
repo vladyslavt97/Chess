77e6dcf5b48ae147d4be09db655df8a04226a6e1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-01-25 11:44:00 +0100

    what should be sent to the client?

--- server/server.cjs
+++ server/server.cjs
@@ -1,25 +1,51 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
+// const path = require("path");
 const { PORT } = process.env;
 
 const {Chess} = require('chess.js')
-
-const chess = new Chess('r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - - 0 19')
+const FEN = 'rnbqkbnr/pppp1ppp/8/4p3/4PP2/8/PPPP2PP/RNBQKBNR b KQkq - 0 2';
+const chess = new Chess(FEN)
 
 // while (!chess.isGameOver()) {
 //   const moves = chess.move();
   //   const move = moves[Math.floor(Math.random() * moves.length)]
 //   chess.move(move)
 // }
-console.log('chess', chess.board());
+// console.log('chess', chess.board());
 console.log('ascii', chess.ascii());
 // console.log('clear', chess.clear());//clears the board
 console.log('fen', chess.fen());
 console.log('get', chess.get('a7')); //get(square) = returns the piece on the square
 // console.log(chess.put({ type: chess.PAWN, color: chess.BLACK }, 'a5')); // put a black pawn on a5; ?????
 console.log('h: ', chess.history()); // returns the history of the current game
+console.log('in check: ', chess.inCheck());//boolean
+console.log('mate?: ', chess.isCheckmate());
+console.log('is attacked: ', chess.isAttacked('e5'));//true if the square is attacked by any piece of the given color.???
+console.log('undo: ', chess.undo());//undo a move!
+// console.log('move: ', chess.move(''));
+function getState(){
+  return chess.ascii();
+}
+// app.use(express.json());
 
+// function convertToBoard(){
+//   let board = chess.board();
+//   let loopOverRows = board.map(el => el);
+//   console.log('loopOverRows', loopOverRows);
+// };
+
+// const state = getState();
+// const fenWithoutSpace = state.split('.').join('').split(' ').join('').split('|').join('');
+// console.log('fenWithoutSpace: ', fenWithoutSpace);
+app.get('/api/gamestate', (req, res) => {
+  console.log('+');
+  const state = chess.board();
+  console.log('stateee: ', state);
+  res.json({st: state});
+
+});
 
 app.listen(PORT, function () {
     console.log(`Express server listening on port ${PORT}`);
--- src/chessboard/Chessboard.tsx
+++ src/chessboard/Chessboard.tsx
@@ -1,25 +1,46 @@
 import "./Chessboard.css"
-import Chess from 'chess.js';
-import { useRef, useState } from "react";
+import { Chess } from "chess.js";
+import { useEffect, useRef, useState } from "react";
 
 export default function ChessBoard() {
-    console.log('chess: ', Chess);
-    const [fen, setFen] = useState(FEN);
-    const { current: chess } = useRef(new Chess(fen));
+    const [fen, setFen] = useState('');
+    // const { current: chess } = useRef(new Chess(fen));
     
-    let rows = [];
-    for (let i = 0; i < 8; i++) {
-        let row = [];
-        for (let j = 0; j < 8; j++) {
-        row.push(<div key={i + '-' + j} className={i % 2 === j % 2 ? 'white' : 'black'}></div>);
-        }
-    rows.push(<div key={i} className="row">{row}</div>);
-  }
+
+    useEffect(()=>{
+        fetch('/api/gamestate')
+        .then((response) => response.json()
+        )
+        .then((data) => {
+            console.log('GET Gamestate: ', data.st);
+            setFen(data.st);
+        })
+        .catch((error) => {
+            console.error('Error caught:', error);
+        });
+    }, [])
+    
+    console.log('fen: ', fen);
+    
+    const gettingRows = () =>{
+        
+    }
+    // console.log('#: ', chess); 
+
+    // let rows = [];
+    // for (let i = 0; i < 8; i++) {
+    //     let row = [];
+    //     for (let j = 0; j < 8; j++) {
+    //     row.push(<div key={i + '-' + j} className={i % 2 === j % 2 ? 'white' : 'black'}></div>);
+    //     }
+    //     rows.push(<div key={i} className="row">{row}</div>);
+    // }
 
   return <div>
             <h1>ChessBorad component</h1>
             <div className="chess-board">
-                <div id="all-cells">{rows}</div>
+                {/* <div id="all-cells">{rows}</div> */}
+                {/* <div id="fen">{fen}</div> */}
             </div>
   </div>
 }
