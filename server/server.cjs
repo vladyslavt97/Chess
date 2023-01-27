de8dd236f881fd46d164d0ba028dcbdfd0bd6471
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-01-27 22:01:24 +0100

    After refactoring. WITH IO setup and cookie, but..

--- server/server.cjs
+++ server/server.cjs
@@ -1,29 +1,41 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const { PORT } = process.env;
+const { PORT, WEB_URL } = process.env;
+const {cookieSession } = require('./cookiesession.cjs');
+
 app.use(express.json());
 const {Chess} = require('chess.js')
 const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 const chess = new Chess(FEN)
 
-// while (!chess.isGameOver()) {
-//   const moves = chess.move();
-  //   const move = moves[Math.floor(Math.random() * moves.length)]
-//   chess.move(move)
-// }
-// console.log('chess', chess.board());
-// console.log('ascii', chess.ascii());
-// console.log('clear', chess.clear());//clears the board
-// console.log('fen', chess.fen());
-// console.log('get', chess.get('a7')); //get(square) = returns the piece on the square
-// console.log(chess.put({ type: chess.PAWN, color: chess.BLACK }, 'a5')); // put a black pawn on a5; ?????
-// console.log('h: ', chess.history()); // returns the history of the current game
-// console.log('in check: ', chess.inCheck());//boolean
-// console.log('mate?: ', chess.isCheckmate());
-// console.log('is attacked: ', chess.isAttacked('e5'));//true if the square is attacked by any piece of the given color.???
-// console.log('undo: ', chess.undo());//undo a move!
-// console.log('move: ', chess.move(''));
+// ------------------------------------ SOCKET  ------------------------------------ //
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+    allowRequest: (req, callback) =>
+        callback(null, req.headers.referer.startsWith(WEB_URL))
+});
+
+io.use((socket, next) => {
+    cookieSession(socket.request, socket.request.res, next);
+});
+let usersConnectedInfo = [];
+io.on("connection", async (socket) => {
+    console.log("[social:socket] incoming socket connection", socket.id);
+
+    //check if the user is signed in.
+    const { userId } = socket.request.session;
+    if (!userId) { // I am not going to send data if a user is not signed in
+        return socket.disconnect(true);
+    }
+
+    
+    socket.on("disconnect", () => {
+        console.log(socket.id, '= should disappear from the list on onlinne users');
+
+    });
+});
+// ------------------------------------ end of socket setup  ------------------------------------ //
 
 
 app.get('/api/gamestate', (req, res) => {
@@ -65,38 +77,10 @@ app.post('/api/movepiece', (req, res) => {
   
 });
 
+// app.listen(PORT, function () {
+//     console.log(`Express server listening on port ${PORT}`);
+// });
 
-
-// ------------------------------------ SOCKET  ------------------------------------ //
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-    allowRequest: (req, callback) =>
-        callback(null, req.headers.referer.startsWith(WEB_URL))
-});
-
-io.use((socket, next) => {
-    cookieSession(socket.request, socket.request.res, next);
-});
-let usersConnectedInfo = [];
-io.on("connection", async (socket) => {
-  console.log("[social:socket] incoming socket connection", socket.id);
-    
-  //check if the user is signed in.
-  const { userId } = socket.request.session;
-  if (!userId) { // I am not going to send data if a user is not signed in
-    return socket.disconnect(true);
-  }
-
-  // emit the board to dispatch the action (maybe get it from DB if its for a logged in user)
-  // send the board to the client who has just connected
-  let board = chess.board().reverse();
-  socket.emit('theBoard', board);
-
-  socket.on("disconnect", () => {
-    console.log(socket.id, '= should disappear from the list on onlinne users');
-  });
-});
-// ------------------------------------ end of socket setup  ------------------------------------ //
 server.listen(PORT, function () {
     console.log(`Express server listening on port ${PORT}`);
 });
