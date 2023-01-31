1691a6b5022c5355c6b4972f64c77964516b80a1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-01-31 11:54:45 +0100

    try again

--- server/cookiesession.cjs
+++ server/cookiesession.cjs
@@ -2,7 +2,6 @@ const {SESSION_SECRET} = process.env;
 const cookieSession = require("cookie-session");
 
 module.exports.cookieSession = cookieSession({
-    // secret: SESSION_SECRET,
     keys: [SESSION_SECRET],
     maxAge: 1000*60*60*24*14
 });
